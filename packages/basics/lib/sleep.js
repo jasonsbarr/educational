967b41f09be513dca0b016e584a9c33e53f90a15
Jason Barr <jason@jasonsbarr.com>
2021-12-01 18:51:20 -0600

    Fix sleep function to block the main thread

--- packages/basics/lib/sleep.js
+++ packages/basics/lib/sleep.js
@@ -1,1 +1,8 @@
-export const sleep = (fn, delay) => setTimeout(fn, delay * 1000);
+// delay is in seconds
+export const sleep = (delay) => {
+  let current;
+  const now = Date.now();
+  do {
+    current = Date.now();
+  } while (current - date < delay * 1000);
+};
