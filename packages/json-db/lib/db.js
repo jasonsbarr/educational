f10484ac30a90dbe14cf56fabc854185ffcc2d76
Jason Barr <jason.barr@metronetinc.com>
2021-12-02 15:13:11 -0600

    Implement select and get methods on DB

--- packages/json-db/lib/db.js
+++ packages/json-db/lib/db.js
@@ -3,10 +3,10 @@ import { readFileSync } from "@jasonsbarr/io";
 
 class DB {
   constructor(filePath) {
-    this.data = parse(readFileSync(filePath));
-    this.query = null;
+    this.db = parse(readFileSync(filePath));
+    this.query = {};
 
-    if (typeof this.data === string) {
+    if (typeof this.db === string) {
       throw new Error("Unable to initialize DB; must be a valid JSON file");
     }
   }
@@ -15,15 +15,37 @@ class DB {
   // the chain of query methods should always start with
   // this when reading, updating, or deleting
   from(table) {
-    const data = prop(table, this.data);
+    const data = prop(table, this.db);
 
     if (!data) {
       throw new Error(`Property ${table} does not exist in JSON DB`);
     }
 
-    this.query = data;
+    this.query.table = table;
+    this.query.data = data;
 
     return this;
+  }
+
+  // Either "*" or a comma-separated string of fields
+  select(fields) {
+    if (fields === "*") {
+      this.query.fields = "*";
+      return this;
+    }
+
+    const fieldsArr = fields.split(",").map((s) => s.trim());
+    this.query.data = this.query.data.map((obj) => {
+      const o = {};
+      fieldsArr.forEach((f) => (o[f] = prop(f, obj)));
+      return o;
+    });
+    this.query.fields = fieldsArr;
+    return this;
+  }
+
+  get() {
+    return this.query.data;
   }
 }
 
