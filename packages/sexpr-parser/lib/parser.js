ea6ded64588b04ca99f5566136f1f4ef25d7a54d
Jason Barr <jason@jasonsbarr.com>
2021-12-03 13:44:29 -0600

    Implement readAtom

--- packages/sexpr-parser/lib/parser.js
+++ packages/sexpr-parser/lib/parser.js
@@ -23,6 +23,8 @@ class Reader {
   }
 }
 
+const isHexadecimalChar = (ch) => /^[0-9a-fA-F]$/.test(ch);
+
 const tokenize = (str) => {
   const re =
     /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]*)/g;
@@ -37,6 +39,9 @@ const tokenize = (str) => {
   return results;
 };
 
+const _keyword = (obj) => (_keyword_Q(obj) ? obj : "\u029e" + obj);
+const _keyword_Q = (obj) => typeof obj === "string" && obj[0] === "\u029e";
+
 const makeVector = (list) => {
   list.unshift(Symbol.for("vector"));
   return list;
@@ -47,11 +52,137 @@ const makeMap = (list) => {
   return list;
 };
 
-const readAtom = (reader) => {};
+const readAtom = (reader) => {
+  let token = reader.next();
+  let i = 0;
+
+  const readWhile = (predicate) => {
+    let str = "";
+    while (predicate(token[i])) {
+      str += token[i++];
+    }
+    return str;
+  };
+
+  const readEscapeSequence = (ch) => {
+    let str = "";
+    let seq = "";
+    if (ch == "n") {
+      str += "\n";
+    } else if (ch == "b") {
+      str += "\b";
+    } else if (ch == "f") {
+      str += "\f";
+    } else if (ch == "r") {
+      str += "\r";
+    } else if (ch == "t") {
+      str += "\t";
+    } else if (ch == "v") {
+      str += "\v";
+    } else if (ch == "0") {
+      str += "\0";
+    } else if (ch == "'") {
+      str += "'";
+    } else if (ch == '"') {
+      str += '"';
+    } else if (ch == "\\") {
+      str += "\\";
+    } else if (ch == "x") {
+      // is hexadecimal escape sequence
+      seq = readWhile((ch) => isHexadecimalChar(ch));
+      str += String.fromCharCode(parseInt(seq, 16));
+    } else if (ch == "u") {
+      // is Unicode escape sequence
+      seq = readWhile((ch) => isHexadecimalChar(ch));
+      str += String.fromCodePoint(parseInt(seq, 16));
+    }
+    return str;
+  };
+
+  const readEscaped = (token) => {
+    let escaped = false;
+    let str = "";
+
+    while (token[i]) {
+      let ch = token[i++];
+
+      if (escaped) {
+        str += readEscapeSequence(ch);
+        escaped = false;
+      } else if (ch == "\\") {
+        escaped = true;
+      } else {
+        str += ch;
+      }
+    }
+
+    return str;
+  };
+
+  if (token.match(/^-?[0-9]+$/)) {
+    return parseInt(token, 10);
+  }
+
+  if (token.match(/^[+-]?[1-9][0-9]*\.[0-9]*/)) {
+    return parseFloat(token);
+  }
+
+  if (token.match(/^"(?:\\.|[^\\"])*"$/)) {
+    token = token.slice(1, -1);
+    return readEscaped(token);
+  }
+
+  if (token[0] === '"') {
+    throw new Error("expected '\"', got EOF");
+  }
+
+  if (token[0] === ":") {
+    return _keyword(token.slice(1));
+  }
+
+  if (token === "nil") {
+    return null;
+  }
+
+  if (token === "true") {
+    return true;
+  }
+
+  if (token === "false") {
+    return false;
+  }
+
+  return Symbol.for(token);
+};
 
 const readList = (reader, start = "(", end = ")") => {};
 
-const readForm = (reader) => {};
+const readForm = (reader) => {
+  const token = reader.peek();
+
+  switch (token) {
+    case ")":
+      throw new Error("Unexpected )");
+
+    case "(":
+      return readList(reader);
+
+    case "]":
+      throw new Error("Unexpected ]");
+
+    case "[":
+      return makeVector(readList(reader, "[", "]"));
+
+    case "}":
+      throw new Error("Unexpected }");
+
+    case "{":
+      return makeMap(readList(reader), "{", "}");
+
+    default:
+      return readAtom(reader);
+  }
+};
 
 const raw = (str) => String.raw`${str}`;
 
