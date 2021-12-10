f2fe4a33271d8c61b42d9a3e896a050f6b1f478c
Jason Barr <jason.barr@metronetinc.com>
2021-12-10 13:36:05 -0600

    Implement router methods

--- packages/api/lib/router.js
+++ packages/api/lib/router.js
@@ -9,8 +9,38 @@ class RouterClass {
     return this._routes;
   }
 
+  delete(route, handler) {
+    this.routes.delete(route, handler);
+
+    return this;
+  }
+
   get(route, handler) {
     this.routes.get(route, handler);
+
+    return this;
+  }
+
+  patch(route, handler) {
+    this.routes.patch(route, handler);
+
+    return this;
+  }
+
+  post(route, handler) {
+    this.routes.post(route, handler);
+
+    return this;
+  }
+
+  put(route, handler) {
+    this.routes.put(route, handler);
+
+    return this;
+  }
+
+  use(...handlers) {
+    this.routes.use(...handlers);
 
     return this;
   }
